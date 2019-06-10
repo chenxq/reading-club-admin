de277c878697c1aa8ec937cf5d664b6863116881
Dina1335 <dina.huang@ringcentral.com>
2019-06-10 16:31:18 +0800

    refactor: remove useless comments

--- src/views/UserList/index.js
+++ src/views/UserList/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import getUserList from '../../actions/userAction';
-//import userReducer from '../../reducers/userReducer';
 
 import { Table } from 'antd';
 
