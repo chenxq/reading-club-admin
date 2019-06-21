53307afbabacf37607bedbd6c49419217aa48f04
chenxq <358009028@qq.com>
2019-06-21 10:28:12 +0800

    delete book

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Modal } from 'antd';
+import { connect } from 'react-redux';
+import deleteBookAction from '../actions/deleteBookAction';
 
 class DeleteModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: true,
-      bookID: 0,
+      visible: props.visible,
+      bookID: props.bookID,
     };
     console.log('init visible state: ', this.state.visible, 'id: ', this.state.bookID);
   };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.visible !== nextProps.visible) {
+      this.setState({ visible: nextProps.visible });
+    }
+  }
 
   showModal = () => {
     this.setState({
@@ -31,6 +39,10 @@ class DeleteModal extends React.Component {
 
   render() {
     console.log('!!!!!!!render delete modal');
+    // this.setState({ bookID: props.bookID, visible: props.visible });
+    // const { bookID, visible } = this.props;
+    // this.setState({ visible: visible });
+    console.log('visible: ', this.state.visible);
     return (
       <Modal
         id={this.state.bookID}
@@ -41,11 +53,24 @@ class DeleteModal extends React.Component {
         okText="确认"
         cancelText="取消"
       >
-        <p>确定删除该书籍吗？`${this.state.bookId}`</p>
+        <p>确定删除该书籍吗？`${this.state.bookID}`</p>
       </Modal>
     );
   };
 }
 
-export default DeleteModal;
+const mapStateToProps = (state) => ({
+  bookID: state.bookID,
+  visible: state.visible
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteBook: (bookID) => deleteBookAction(bookID)(dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DeleteModal);
+// export default DeleteModal;
 // export default connect(mapStateToProps)(DeleteModal);
