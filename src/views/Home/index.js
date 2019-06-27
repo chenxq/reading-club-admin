034e15a7a208c5a9f624ef32862efd519c67d39c
Dina1335 <dina.huang@ringcentral.com>
2019-06-27 09:28:21 +0800

    change loginpage UI

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import authAction from '../../actions/authAction';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import './index.css';
 
 class AuthView extends React.Component {
   constructor(props) {
@@ -37,46 +38,58 @@ class AuthView extends React.Component {
     const { getFieldDecorator } = this.props.form;
     // const { history } = this.props;
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {/* {getFieldDecorator('remember', {
+      <div className="form">
+        <span className="logo">Reading Club</span>
+
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('userName', {
+              rules: [
+                { required: true, message: 'Please input your username!' },
+              ],
+            })(
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [
+                { required: true, message: 'Please input your Password!' },
+              ],
+            })(
+              <Input
+                prefix={
+                  <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {/* {getFieldDecorator('remember', {
             valuePropName: 'checked',
             initialValue: true,
           })(<Checkbox>Remember me</Checkbox>)} */}
-          {/* <a className="login-form-forgot" href="">
+            {/* <a className="login-form-forgot" href="">
             Forgot password
           </a> */}
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            Log in
-          </Button>
-          {/* Or <a href="">register now!</a> */}
-        </Form.Item>
-      </Form>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            {/* Or <a href="">register now!</a> */}
+          </Form.Item>
+        </Form>
+      </div>
     );
   }
 }
