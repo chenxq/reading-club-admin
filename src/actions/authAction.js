956fd1767b924364f450a7e231b432ccc7a37782
Dina1335 <dina.huang@ringcentral.com>
2019-07-03 14:58:45 +0800

    refactor logout

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,8 +1,5 @@
 import authService from '../services/authService';
-import jwt_decode from 'jwt-decode';
 import setAuthToken from '../utils/setAuthToken.js';
-
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 const AUTH_START = 'AUTH_START';
 const AUTH_SUCCESS = 'AUTH_SUCCESS';
@@ -30,9 +27,7 @@ export default function auth(authInfo) {
       localStorage.setItem('jwToken', token);
       //设置axios的headers token
       setAuthToken(token);
-      const decoded = jwt_decode(token);
       dispatch(authAction(AUTH_SUCCESS, ret));
-      dispatch(setCurrentUser(decoded));
     } catch (e) {
       dispatch(authAction(AUTH_FAILURE, { msg: 'Something wrong!' }));
       return;
@@ -40,19 +35,8 @@ export default function auth(authInfo) {
   };
 }
 
-export const setCurrentUser = (decoded) => {
-  // 设置type,下一步return到reducers/authReducer.js中
-  return {
-    type: SET_CURRENT_USER,
-    payload: decoded,
-  };
-};
-
-// 退出功能
-export const logoutUser = () => (dispatch) => {
-  // 移除本地存储信息
+export const logoutUserAction = () => (dispatch) => {
   localStorage.removeItem('jwToken');
   setAuthToken(false);
-  // 分发到reducer中,传空对象后传入reducer就是空对象
-  dispatch(setCurrentUser({}));
+  dispatch(authAction(AUTH_SUCCESS, {}));
 };
