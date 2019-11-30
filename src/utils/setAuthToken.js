6e4a83c0402e2f39aac848841767dbdb68b91fc5
joe zhang <joe.zhang@ringcentral.com>
2019-11-30 10:05:16 +0800

    auth fail bug fix

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const setAuthToken = (token) => {
   if (token) {
     // token存在设置header,
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common['Authorization'] = 'Basic ' + token;
   } else {
     // 没有token就移除
     delete axios.defaults.headers.common['Authorization'];
